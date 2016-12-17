12651ffc50aa5b21ae48b2c8d7283ddfae8e9e2d
gbetanzos <betanzos@gmail.com>
2016-12-16 18:26:35 -0600

    Initial Commit

---
+++ wfaAD/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace wfaAD
+{
+    partial class frmCheckAD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCheckAD));
+            this.txtEmails = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmails
+            // 
+            this.txtEmails.Location = new System.Drawing.Point(12, 41);
+            this.txtEmails.Multiline = true;
+            this.txtEmails.Name = "txtEmails";
+            this.txtEmails.Size = new System.Drawing.Size(188, 381);
+            this.txtEmails.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(12, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(185, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "&Check for users / subscriptions";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(206, 41);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(707, 381);
+            this.txtResults.TabIndex = 3;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(403, 17);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(16, 13);
+            this.lblUser.TabIndex = 4;
+            this.lblUser.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(206, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Check same user";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmCheckAD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 437);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtEmails);
+			//this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCheckAD";
+            this.Text = "Check for AD users";
+            this.Resize += new System.EventHandler(this.frmCheckAD_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmails;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ wfaAD/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace wfaAD
+{
+    public partial class frmCheckAD : Form
+    {
+        int c = 0;
+        DirectoryEntry adEntry = null;
+        public frmCheckAD()
+        {
+            InitializeComponent();
+            SetADInfoAndCredentials();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            this.c = 0;
+            txtEmails.Text = "";
+            SetADInfoAndCredentials();
+            string email = "";
+            
+            List<ADUser> usuarios =getUsersAD();
+            foreach (ADUser u in usuarios) {
+                email = u.Email.Substring(0,u.Email.IndexOf('^')).ToLower();
+                txtEmails.Text +=email +Environment.NewLine; ;
+            }
+            //this.checkSubscriptionsEvent();
+            while (txtResults.Text.Length == 0) {
+                this.checkSubscriptionsEvent();
+                this.c++;
+            }
+        }
+        private void checkSubscriptionsEvent(){
+            string email = "";
+            if (txtEmails.Lines.Length > 0 && c < txtEmails.Lines.Length)
+            {
+                for (int x = 0; x < txtEmails.Lines.Length; x++) {
+                    email = txtEmails.Lines[x];
+                    if (x == this.c) {
+                        try {
+                            checkForSubscriptions(email);
+                            lblUser.Text = email;
+                        }
+                        catch (Exception ex) {
+                            MessageBox.Show(ex.Message.ToString());
+                        }
+                        
+                    }
+                    //SearchForMailInAD(email);
+                    //MessageBox.Show(email);
+                    //lblCount.Text = x.ToString() + " of " + txtEmails.Lines.Length.ToString();
+                    Application.DoEvents();
+                    //System.Threading.Thread.Sleep(1000);
+                }
+                
+            }
+            //MessageBox.Show("Búsqueda terminada"); 
+            lblUser.Text += " Search has FINISHED for this user.";
+        }
+        private List<ADUser> getUsersAD()
+        {
+            List<ADUser> lstADUsers = new List<ADUser>();
+            try
+            {
+                
+				string DomainPath = "LDAP://OU=Disabled Accounts,OU=Disabled_Resource Accounts,DC=CONTOSO,DC=net";
+                DirectoryEntry searchRoot = new DirectoryEntry(DomainPath); 
+                DirectorySearcher search = new DirectorySearcher(searchRoot);
+                //search.Filter = "(&(objectClass=user)(objectCategory=person))";
+                //search.Filter = "(&(objectClass=user)((userAccountControl:1.2.840.113556.1.4.803:=2)))";
+                search.PropertiesToLoad.Add("samaccountname");
+                search.PropertiesToLoad.Add("mail");
+                search.PropertiesToLoad.Add("usergroup");
+                search.PropertiesToLoad.Add("displayname");//first name
+                SearchResult result;
+                SearchResultCollection resultCol = search.FindAll();
+                if (resultCol != null)
+                {
+                    for (int counter = 0; counter < resultCol.Count; counter++)
+                    {
+                        string UserNameEmailString = string.Empty;
+                        result = resultCol[counter];
+                        if (result.Properties.Contains("samaccountname") && 
+                                 result.Properties.Contains("mail") && 
+                            result.Properties.Contains("displayname"))
+                        {
+                            ADUser objSurveyUsers = new ADUser();
+                            objSurveyUsers.Email = (String)result.Properties["mail"][0] + 
+                              "^" + (String)result.Properties["displayname"][0];
+                            objSurveyUsers.UserName = (String)result.Properties["samaccountname"][0];
+                            objSurveyUsers.DisplayName = (String)result.Properties["displayname"][0];
+                            lstADUsers.Add(objSurveyUsers);
+                        }
+                    }
+                }
+                return lstADUsers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return lstADUsers;
+            }
+
+        }
+
+        private void checkForSubscriptions(string email)
+        {
+            txtResults.Text = "";
+            List<string> subscriptions = new List<string>();
+			SqlConnection conn = new SqlConnection("Server=SSRSSQLServerDB;Database=ReportServer;Trusted_Connection=True;");
+            string sql = @"WITH subscriptionXmL
+              AS (
+                   SELECT
+                    SubscriptionID ,
+                    OwnerID ,
+                    Report_OID ,
+                    Locale ,
+                    InactiveFlags ,
+                    ExtensionSettings ,
+                    CONVERT(XML, ExtensionSettings) AS ExtensionSettingsXML ,
+                    ModifiedByID ,
+                    ModifiedDate ,
+                    Description ,
+                    LastStatus ,
+                    EventType ,
+                    MatchData ,
+                    LastRunTime ,
+                    Parameters ,
+                    DeliveryExtension ,
+                    Version
+                   FROM
+                    ReportServer.dbo.Subscriptions
+                 ),
+                     -- Get the settings as pairs
+            SettingsCTE
+              AS (
+                   SELECT
+                    SubscriptionID ,
+                    ExtensionSettings ,
+        -- include other fields if you need them.
+                    ISNULL(Settings.value('(./*:Name/text())[1]', 'nvarchar(1024)'),
+                           'Value') AS SettingName ,
+                    Settings.value('(./*:Value/text())[1]', 'nvarchar(max)') AS SettingValue
+                   FROM
+                    subscriptionXmL
+                    CROSS APPLY subscriptionXmL.ExtensionSettingsXML.nodes('//*:ParameterValue') Queries ( Settings )
+                 )
+        SELECT
+            *
+        FROM
+            SettingsCTE
+        WHERE
+            settingName IN ( 'TO', 'CC', 'BCC' ) AND SettingValue LIKE '%"+email+"%'";
+            SqlDataReader rdr;
+            SqlCommand cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+            rdr = cmd.ExecuteReader();
+            if (rdr.HasRows) {
+                while (rdr.Read()) {
+                    txtResults.Text += rdr[0].ToString() + " - " + rdr[3].ToString()+Environment.NewLine;
+                    subscriptions.Add(rdr[0].ToString());
+                }
+            }
+            rdr.Close();
+            foreach (string s in subscriptions) {
+                sql = @"SELECT CAT.[Path] AS ReportPath 
+                        FROM dbo.Subscriptions AS SUB 
+                             INNER JOIN dbo.Users AS USR 
+                                 ON SUB.OwnerID = USR.UserID 
+                             INNER JOIN dbo.[Catalog] AS CAT 
+                                 ON SUB.Report_OID = CAT.ItemID 
+                             INNER JOIN dbo.ReportSchedule AS RS 
+                                 ON SUB.Report_OID = RS.ReportID 
+                                    AND SUB.SubscriptionID = RS.SubscriptionID 
+                             INNER JOIN dbo.Schedule AS SCH 
+                                 ON RS.ScheduleID = SCH.ScheduleID 
+                        WHERE SUB.SubscriptionID ='"+s+"'";
+                cmd = new SqlCommand(sql, conn);
+                string reportPath=cmd.ExecuteScalar().ToString();
+                if (reportPath.Length > 0) {
+                    txtResults.Text += reportPath + Environment.NewLine;
+                }
+            }
+            conn.Close();
+        }
+        private void SetADInfoAndCredentials()
+        {
+			adEntry = new DirectoryEntry("DC=CONTOSO,DC=net", 
+				"usuario", "password");
+        }
+
+        
+
+        private void dgvSubscriptions_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmCheckAD_Resize(object sender, EventArgs e)
+        {
+            txtEmails.Height = this.Height - 75;
+            txtResults.Width = this.Width - txtResults.Left - 25;
+            txtResults.Height = this.Height - txtResults.Top - 35;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.checkSubscriptionsEvent();
+            this.c++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.c--;
+            this.checkSubscriptionsEvent();
+        }
+    }
+    public class ADUser
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public bool isMapped { get; set; }
+    }
+}
